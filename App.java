03255cc6fcfb0806d152efa1ee53721939bc35fe
LucasFabiano1 <lucasfasouza11@gmail.com>
2023-03-21 14:57:15 -0300

    21/03

--- App.java
+++ App.java
@@ -2,6 +2,7 @@ package AulaRevisão;
 
 public class App {
     public static void main(String[] args) {
-        
+        ExemplosRevisão1 obj1 = new ExemplosRevisão1();
+        obj1.exeercicio1();
     }
 }
--- ExemplosRevisão1.java
+++ ExemplosRevisão1.java
@@ -1,4 +1,28 @@
 package AulaRevisão;
+
+import java.util.Scanner;
+
 public class ExemplosRevisão1 {
-    
+    Scanner sc = new Scanner (System.in);
+    public void exeercicio1() {
+        System.out.println("Informe a primeira nota do aluno");
+        int nota1 = sc.nextInt();
+        System.out.println("Informe a segunda nota");
+        int nota2 = sc.nextInt();
+        double media = (nota1+nota2)/2 ;
+        System.out.println("A média do aluno é " +media);
+        System.out.println("Informe o total de aulas da disiplina");
+        int aulasDadas= sc.nextInt();
+        System.out.println("Informe a quantidade de faltas do auluno");
+        int totalFaltas = sc.nextInt();
+        double frequencia= (aulasDadas -totalFaltas)*100/aulasDadas;
+        System.out.println("O tatal de frequencia é " + frequencia+ "%");
+        if(media>50 && frequencia>=75){
+            System.out.println("Aluno aprovado");
+        } else if(media<50 && frequencia>=75){
+            System.out.println("Aluno de recuperção");
+        } else {
+            System.out.println("Aluno reprovado");
+        }
+    }
 }
